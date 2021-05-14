0e104adcf530813cd9f43eda92e31b751c62fe4f
José Ángel <joseangelmt@outlook.com>
2021-05-14 12:55:51 +0200

    Moved database attributes to Code. digi21/TareasDigi3D#364

--- Digi21.DigiNG.IO.BinDouble/BinDouble.cs
+++ Digi21.DigiNG.IO.BinDouble/BinDouble.cs
@@ -36,13 +36,11 @@ namespace Digi21.DigiNG.IO.BinDouble
         public ReadOnlyPolygon Add(Polygon polygon) => throw null;
         public ReadOnlyText Add(Text text) => throw null;
         public void Add(IEnumerable<Entity> entities) => throw null;
-        public void Add(Entity entity, IDictionary<string, IDictionary<string, object>> databaseAttributes) => throw null;
         internal void AñadeResultado(string resultado) => throw null;
         internal void AñadeTarea(ITask task) => throw null;
         public void Delete(Entity entity) => throw null;
         public void Delete(IEnumerable<Entity> entities) => throw null;
         public void Dispose() => throw null;
-        public IDictionary<string, IDictionary<string, object>> GetDatabaseAttributes(Entity entity) => throw null;
         public IEnumerator<Entity> GetEnumerator() => throw null;
         IEnumerator IEnumerable.GetEnumerator() => throw null;
         public string Wkt => throw null;
